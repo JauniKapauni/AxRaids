fd407a08913a809fd389862a848e1c62688c59ea
JauniKapauni <riches-goods.54@icloud.com>
2026-07-17 18:24:26 +0200

    fix(listener): add player feedback message

--- src/main/java/de/jaunikapauni/axraids/listener/RaidTriggerEventListener.java
+++ src/main/java/de/jaunikapauni/axraids/listener/RaidTriggerEventListener.java
@@ -1,6 +1,7 @@
 package de.jaunikapauni.axraids.listener;
 
 import de.jaunikapauni.axraids.AxRaids;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.raid.RaidTriggerEvent;
@@ -15,6 +16,7 @@ public class RaidTriggerEventListener implements Listener {
     public void onRaid(RaidTriggerEvent e){
         if(!reference.isRaidsEnabled()){
             e.setCancelled(true);
+            e.getPlayer().sendMessage(ChatColor.RED + "Raids are disabled!");
         }
     }
 }
